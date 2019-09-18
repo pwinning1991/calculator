9f08f94b5356440a58167caedc3fd33d3844b4ae
pwinning1991 <ftcpjw@yahoo.com>
2019-09-17 21:20:56 -0400

    added subtract tests

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -4,12 +4,14 @@ from calculator import calc
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
-        result = calc.add(10,5)
-        self.assertEqual(result, 15)
+        self.assertEqual(calc.add(10,5), 15)
+        self.assertEqual(calc.add(-1,1), 0)
+        self.assertEqual(calc.add(-5,-5), -10)
 
     def test_subtract(self):
-        result = calc.subtract(10,5)
-        self.assertEqual(result, 5)
+        self.assertEqual(calc.subtract(10,5), 5)
+        self.assertEqual(calc.subtract(-10,-5), -5)
+        self.assertEqual(calc.subtract(1,1), 0)
 
 if __name__ == '__main__':
     unittest.main()
