52c9084811e44d05557cc7593d898e96cda161a8
pwinning1991 <ftcpjw@yahoo.com>
2019-09-17 21:25:15 -0400

    added multiply function

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -13,5 +13,10 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.subtract(-10,-5), -5)
         self.assertEqual(calc.subtract(1,1), 0)
 
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(10,5), 50)
+        self.assertEqual(calc.multiply(-2, 2), -4)
+        self.assertEqual(calc.multiply(100,0), 0)
+
 if __name__ == '__main__':
     unittest.main()
