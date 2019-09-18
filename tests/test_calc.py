73c558df6bd7a72be0ffb56ee94c86c42d19a73c
pwinning1991 <ftcpjw@yahoo.com>
2019-09-17 21:39:31 -0400

    added subtract funtion

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -18,5 +18,12 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calc.multiply(-2, 2), -4)
         self.assertEqual(calc.multiply(100,0), 0)
 
+    def test_divide(self):
+        self.assertEqual(calc.divide(10,5), 2)
+        self.assertEqual(calc.divide(-2, 2), -1)
+        self.assertEqual(calc.divide(5,2), 2.5)
+        with self.assertRaises(ValueError):
+            calc.divide(10, 0)
+
 if __name__ == '__main__':
     unittest.main()
